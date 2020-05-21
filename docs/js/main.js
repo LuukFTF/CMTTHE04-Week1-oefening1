7eb606fd86cbbddf65cc9ac2d6a718f782eb4ae2
LuukFTF <lucasvdvegt@gmail.com>
2020-05-21 13:43:25 +0200

    homework done

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,50 @@
-//
-// vis element
-//
-let fish = document.getElementsByTagName("fish")[0]
-fish.style.transform = "translate(200px, 100px)"
-fish.style.filter = "hue-rotate(200deg)"
+let game = document.getElementsByTagName("game")[0]
 
-//
-// bubble element
-//
-let bubble = document.getElementsByTagName("bubble")[0]
-bubble.style.transform = "translate(20vw, 100vh)"
+function createFish() {
+    let fish = document.createElement("fish")
+    game.appendChild(fish)
+    
+    let randomPosx = Math.random() * window.innerWidth
+    let randomPosy = Math.random() * window.innerHeight
+    let randomHuedeg = Math.random() * 360
+
+    let posx = randomPosx
+    let posy = randomPosy
+    let huedeg = randomHuedeg
+
+    fish.style.transform = `translate(${posx}px, ${posy}px)`
+    fish.style.filter = `hue-rotate(${huedeg}deg)`
+
+    fish.addEventListener("click", clickDead) 
+}
+
+
+function createBubble() {
+    let bubble = document.createElement("bubble")
+
+    let randomPosx = Math.random() * window.innerWidth
+    let randomPosy = Math.random() * window.innerHeight
+    
+    let posx = randomPosx
+    let posy = randomPosy
+
+    bubble.style.transform = `translate(${posx}px, ${posy}px)`
+
+    bubble.addEventListener("click", clickRemove) 
+
+    game.appendChild(bubble)
+}
+
+function clickDead(){
+    this.classList.add("dead")
+}
+
+function clickRemove(){
+    this.remove()
+}
+
+
+for (let i = 0; i < 100; i++) {
+    createBubble()
+    createFish()
+}
